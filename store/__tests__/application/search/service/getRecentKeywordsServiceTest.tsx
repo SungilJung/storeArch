eda8f1ecc2614fafceff96712b94e84e94374753
bambam <green5940@gmail.com>
2021-12-13 21:33:03 +0900

    mock 생성하여 테스트 진행

--- store/__tests__/application/search/service/getRecentKeywordsServiceTest.tsx
+++ store/__tests__/application/search/service/getRecentKeywordsServiceTest.tsx
@@ -1,17 +1,18 @@
 
     
+import { GetKeywordsPort } from "../../../../src/application/search/application/port/out/getKeywordsPort";
 import GetRecentKeywordsService from "../../../../src/application/search/application/service/getRecentKeywordsService";
 
 describe('getRecentKeywordsQuery', () => {
     it('get recent keywords', async () => {
         
-        const loadDatas = (key: string): Promise<string> => {                
+        const MockKeywordPersistanceAdapter:GetKeywordsPort = (key: string): Promise<string> => {                
             return new Promise((resolve) => {
                 resolve('data');
             });
         };
 
-        let result = await GetRecentKeywordsService('key', loadDatas);
+        let result = await GetRecentKeywordsService('key', MockKeywordPersistanceAdapter);
         let expected = 'data';
             
         expect(result).toEqual(expected);
